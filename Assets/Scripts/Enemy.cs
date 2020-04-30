853f12b01671670bfbfb29ed4cb100fe3bd15b59
Jenni <justlittleoldmejenn@gmail.com>
2020-04-29 23:37:42 -0700

    Fixing Score
    Score has word score but doesn't update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
     private Rigidbody enemyRb;
     private GameObject player;
+    private int score = 0;
+    [SerializeField] public Text ScoreText;
 
     [SerializeField] private float speed = 3.0f;
 
@@ -12,11 +15,13 @@ public class Enemy : MonoBehaviour {
 
     private void OnDestroy() {
         SpawnManager.Instance.UpdateEnemyCount(-1);
+        SetScoreText();
     }
 
     private void Start() {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+        
     }
 
     private void Update() {
@@ -25,6 +30,12 @@ public class Enemy : MonoBehaviour {
 
         if(transform.position.y <= -10) {
             Destroy(gameObject);
+            score++;
+            SetScoreText();
         }
+    }
+
+void SetScoreText() {
+        ScoreText.text = "Score: " + score.ToString();
     }
 }
