b5a6ac1eca06de57b97e632bee0ce460e4a17c0f
Jennifer Felton <justlittleoldmejenn@gmail.com>
2020-05-21 16:53:59 -0700

    Trying To Add Score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class Enemy : MonoBehaviour {
     private Rigidbody enemyRb;
     private GameObject player;
     private int score = 0;
-    [SerializeField] private Text scoreText;
-    
+    [SerializeField] private TextMeshProUGUI scoreText;
+
 
     [SerializeField] private float speed = 3.0f;
 
@@ -16,29 +18,29 @@ public class Enemy : MonoBehaviour {
 
     private void OnDestroy() {
         SpawnManager.Instance.UpdateEnemyCount(-1);
-        SetScoreText();
+       
     }
 
     private void Start() {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
-        
-        
+        scoreText.text = "Score: " + score;
+
+
     }
 
     private void Update() {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
 
-        if(transform.position.y <= -10) {
+        if (transform.position.y <= -10) {
             Destroy(gameObject);
             score++;
-            SetScoreText();
+            scoreText.text = "Score: " + score;
+
         }
     }
 
-void SetScoreText() {
-        scoreText.text = "Score: " + score.ToString();
-       
-    }
+    
+
 }
